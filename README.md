99c24969a6dd68ce964747b1206a3b18d050c1a6
lovecycy <96785236+lovecycy@users.noreply.github.com>
2024-04-21 19:41:46 +0800

    添加头部工具栏，配置css模块化作用域

---
+++ README.md
@@ -0,0 +1,13 @@
+# CO-PAINT
+
+> 协同绘制项目
+
+## 开始使用
+
+
+```shell
+# 仅支持使用pnpm安装
+pnpm install
+# 启动项目
+pnpm start
+```
---
+++ projects/client/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <header className='header'>
+        todo
+      </header>
+      <aside>
+        todo
+      </aside>
+      <main className='main'>
+        todo
+      </main>
+    </div>
+  )
+}
+
+export default App
